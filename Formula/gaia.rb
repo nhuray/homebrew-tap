8108aad9d3000953bbc114ece81e1cdf053fc111
Nicolas Huray <nhuray@bandsintown.com>
2017-12-09 10:12:15 -0500

    Update gaia.rb

--- Formula/gaia.rb
+++ Formula/gaia.rb
@@ -1,9 +1,9 @@
 class Gaia < Formula
   desc "A wrapper script for Terraform"
   homepage "https://github.com/bandsintown/gaia"
-  url "https://github.com/bandsintown/gaia/archive/v0.0.4.tar.gz"
-  version "v0.0.4"
-  sha256 "235f244f4362964defb1dfaa57722655b8e2661b3ec226148913714a47747126"
+  url "https://github.com/bandsintown/gaia/archive/v0.0.5.tar.gz"
+  version "v0.0.5"
+  sha256 "517ee814901dd53214c51673272ff170f1aabed61f62ac26a02e4bd4ffca52af"
 
   def install
     bin.install "bin/gaia"
