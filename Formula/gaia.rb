53c3a179acb2918a60060d2c0477762ce4dd2962
Nicolas Huray <nicolas.huray@gmail.com>
2017-12-09 11:43:15 -0500

    Update gaia.rb formula (version v0.0.6)

--- Formula/gaia.rb
+++ Formula/gaia.rb
@@ -3,7 +3,7 @@ class Gaia < Formula
   homepage "https://github.com/bandsintown/gaia"
   url "https://github.com/bandsintown/gaia/archive/v0.0.6.tar.gz"
   version "v0.0.6"
-  sha256 "e9709ce4c71414fd0476ee9e9d59bc04c0d773ef974a5f5e2cae0c281451ba14"
+  sha256 "8a51df074ac75f350af346c304ef575828cc2ddc068ef6c23b9ac34fbe014a0b"
 
   def install
     bin.install "bin/gaia"
