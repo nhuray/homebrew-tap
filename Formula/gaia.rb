dde5877774031ff3ee4693dba2fdc0f344a8199d
Maria F. Soto <msotoperdomo@bandsintown.com>
2018-01-24 10:41:49 -0500

    Update gaia.rb formula (version v0.0.11)

--- Formula/gaia.rb
+++ Formula/gaia.rb
@@ -1,9 +1,9 @@
 class Gaia < Formula
   desc "A wrapper script for Terraform"
   homepage "https://github.com/bandsintown/gaia"
-  url "https://github.com/bandsintown/gaia/archive/v0.0.10.tar.gz"
-  version "v0.0.10"
-  sha256 "297b66b717467d935a8434dd6c9bfff1247176ab9420a13100351ca28dbc0116"
+  url "https://github.com/bandsintown/gaia/archive/v0.0.11.tar.gz"
+  version "v0.0.11"
+  sha256 "2d99b0fd55524b0b36fa5d20abd7ee2e1be4a708af8c62fe3a152cd21674dba1"
 
   def install
     bin.install "bin/gaia"
